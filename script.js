e598b1f2e65c6c6ee3fba13049d500c193fc3188
JohnW1therSpoon101 <sternenterprise12@gmail.com>
2025-09-27 15:32:02 -0400

    Finished Lab 5

--- script.js
+++ script.js
@@ -1,33 +1,68 @@
-//step 2
+// Setup Array
 let myArray = [10, "banana", true];
 console.log(myArray);
 
-//step 2a (adding new elements with push)
+// adding new elements with push
 myArray.push([1, 2, 3]);
 myArray.push("added string");
 console.log(myArray);
 
-myArray[2] = "something new";
-
-//step 2b remove elements using shift
+// remove elements using shift
 myArray.shift();
 console.log(myArray);
 
-//step 2c change 2nd element using
+// change 2nd element using
 myArray[1] = 345;
 console.log(myArray);
 
-//step 2d replace last one with "false"
-myArray[myArray.lenghth - 1] = false;
+// replace last one with "false"
+myArray[myArray.length - 1] = false;
 console.log(myArray);
 
-//step 3
+// new while loop
 let i = 0;
 while (i < myArray.length) {
   console.log(myArray[i]);
   i++;
 }
 
-//step 5
+//modded array = let myArray = [ "banana", 345, [1, 2, 3], false];
 
-//step 6
+// print data type of each using while loop
+let j = 0;
+while (j < myArray.length) {
+  console.log(typeof myArray[j]);
+  j++;
+}
+
+//MUSIC APPLICATION
+// Create another array w/ elements 7 elements
+let myScale = ["C", "D", "E", "F", "G", "A", "B"];
+console.log(myScale);
+
+// Print data types of each element
+let k = 0;
+while (k < myScale.length) {
+  console.log(typeof myScale[k]);
+  k++;
+}
+
+// print the entire array
+
+console.log(myScale);
+
+// modify myScale values
+let m = 0;
+while (m < myScale.length) {
+  if (myScale[m] === "F") {
+    myScale[m] = "F#"; //replacing F with F#
+  } else if (myScale[m] === "B") {
+    myScale[m] = "Bb"; //replacing B with Bb
+  }
+  m++;
+}
+
+// print modified scale
+console.log(myScale);
+
+//END OF PROJECT !
